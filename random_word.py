a725cd3da5ed4816fd84240df4e4f4b250d6441a
Pawel <pawel.fran@gmail.com>
2020-03-08 16:26:08 +0100

    start / end to random word

--- random_word.py
+++ random_word.py
@@ -4,37 +4,59 @@ import re
 
 # todo option to skip words already asked
 # take only some portion of all words (random)
-# take only first or last n words
+# add flags for start / end
+# do not use latin as default dict
 # filter out lines starting with '#'
 
-def parse_dict(lines):
+def parse_dict(lines, start=None, end=None):
+
+	print (start)
+	print(end)
 	dict = {}
 
 	current_key = None
 	current_values = []
 
+	if start == None:
+		start = 1
+	if end == None:
+		end = len(lines) # always greater or equal # of words in dictionary
+
+	current_element = 1
 	for line in lines:
-		if not line[0].isdigit():
-			dict[current_key] = current_values
-			current_key = line
-			current_values = []
-		else:
-			current_values.append(line)
+		if current_element >= start and current_element <= end:
+			if not line[0].isdigit():
+				current_element = current_element + 1
+				dict[current_key] = current_values
+				current_key = line
+				current_values = []
+			else:
+				current_values.append(line)
 
 	del dict[None]
 
 	return dict
 
-
+start = None
+end = None
 if len(sys.argv) == 1:
 	file_name = 'latin.txt'
+elif len(sys.argv) == 2:
+	file_name = "{}.txt".format(sys.argv[1])
+elif len(sys.argv) == 3:
+	file_name = 'latin.txt'
+	start = sys.argv[1]
+	end = sys.argv[2]
 else:
 	file_name = "{}.txt".format(sys.argv[1])
+	start = sys.argv[2]
+	end = sys.argv[3]
 
 lines_raw = open(file_name, "r").readlines()
 lines = [line.strip() for line in lines_raw if len(line.rstrip()) > 0]
 
-dict = parse_dict(lines)
+dict = parse_dict(lines, int(start), int(end))
+print (dict)
 
 while(True):
 	word = random.choice(list(dict.keys()))
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 # random_word.py
-# should be run like 'python random_word.py <dict>' where <dict> may be latin or eng or nothing (then latin)
+# should be run like 'python random_word.py <dict> <start> <end>' where <dict> may be latin or eng or nothing (then latin) and start / end are number of words to include (left inclusive, right not)
 
 # declension.py
 # should be run like 'python declension.py' optionally followed by list of declensions to include ex. 'python declension.py first third mixed'
