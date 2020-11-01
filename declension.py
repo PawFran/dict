d623053e8336c8c6d5fe72069ae052275dcb5dc7
Pawel <pawel.fran@gmail.com>
2020-11-01 11:10:59 +0100

    add if __main__

--- declension.py
+++ declension.py
@@ -20,49 +20,50 @@ def weak_contains(lst, x):
 	return any([s in x for s in lst])
 
 
-declensions_to_include = sys.argv[1:]
+if __name__ == '__main__':
+	declensions_to_include = sys.argv[1:]
 
-file = open("declension.json")
-dec_raw = json.load(file)
-file.close()
+	file = open("declension.json")
+	dec_raw = json.load(file)
+	file.close()
 
-if len(declensions_to_include) == 0:
-	dec = dec_raw
-else:
-	dec = {base_word: dec_raw[base_word] for base_word in dec_raw.keys() if weak_contains(declensions_to_include, list(dec_raw[base_word].keys())[0])}
-
-while(len(dec) > 0):
-	base_word = random.choice(list(dec.keys()))
-	declension_number = random.choice(list(dec[base_word]))
-	genre = random.choice(list(dec[base_word][declension_number]))
-	number = random.choice(list(dec[base_word][declension_number][genre].keys()))
-	case = random.choice(list(dec[base_word][declension_number][genre][number].keys()))
-
-	print('{}: {} {}'.format(base_word, short(case), short(number))) 
-	answer = input("")
-	correct_answer = dec[base_word][declension_number][genre][number][case]
-	if equals_modulo_case_and_unicode(correct_answer, answer):
-		print("correct")
-		print('')
+	if len(declensions_to_include) == 0:
+		dec = dec_raw
 	else:
-		print("wrong. correct answer is {}".format(correct_answer))
-		print('')
+		dec = {base_word: dec_raw[base_word] for base_word in dec_raw.keys() if weak_contains(declensions_to_include, list(dec_raw[base_word].keys())[0])}
 
-# 	feedback = input("")
-# 	if 'x' in feedback:
-# 		del dec[base_word][declension_number][genre][number][case]
-# 		if dec[base_word][declension_number][genre][number] == None:
-# 			del dec[base_word][declension_number][genre][number]
-# 		if dec[base_word][declension_number][genre] == None:
-# 			del dec[base_word][declension_number][genre]
-# 		if dec[base_word][declension_number] == None:
-# 			del dec[base_word][declension_number]
-# 		if dec[base_word] == None:
-# 			del dec[base_word]
-# 		print('')
-# 	if 'c' in feedback:
-# 		print('there are {} words left in current dict'.format(len(dec)))
-# 		print(dec)
-# 		print('')
+	while(len(dec) > 0):
+		base_word = random.choice(list(dec.keys()))
+		declension_number = random.choice(list(dec[base_word]))
+		genre = random.choice(list(dec[base_word][declension_number]))
+		number = random.choice(list(dec[base_word][declension_number][genre].keys()))
+		case = random.choice(list(dec[base_word][declension_number][genre][number].keys()))
 
-print('current dictionary is empty. finishing program')
+		print('{}: {} {}'.format(base_word, short(case), short(number))) 
+		answer = input("")
+		correct_answer = dec[base_word][declension_number][genre][number][case]
+		if equals_modulo_case_and_unicode(correct_answer, answer):
+			print("correct")
+			print('')
+		else:
+			print("wrong. correct answer is {}".format(correct_answer))
+			print('')
+
+	# 	feedback = input("")
+	# 	if 'x' in feedback:
+	# 		del dec[base_word][declension_number][genre][number][case]
+	# 		if dec[base_word][declension_number][genre][number] == None:
+	# 			del dec[base_word][declension_number][genre][number]
+	# 		if dec[base_word][declension_number][genre] == None:
+	# 			del dec[base_word][declension_number][genre]
+	# 		if dec[base_word][declension_number] == None:
+	# 			del dec[base_word][declension_number]
+	# 		if dec[base_word] == None:
+	# 			del dec[base_word]
+	# 		print('')
+	# 	if 'c' in feedback:
+	# 		print('there are {} words left in current dict'.format(len(dec)))
+	# 		print(dec)
+	# 		print('')
+
+	print('current dictionary is empty. finishing program')
